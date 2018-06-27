1df34daa40caf349edda4c190810b6a989392e85
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2018-06-27 17:52:37 -0300

    Finished example test

--- training-app/spec/models/user_spec.rb
+++ training-app/spec/models/user_spec.rb
@@ -4,7 +4,14 @@ describe User do
   
   describe '.initialize' do
     it 'creates an user with first and last names' do
-     
+      user = User.create(
+        email: 'test@wolox.com.ar', 
+        password: '123123123', 
+        password_confirmation: '123123123', 
+        first_name: 'Test', 
+        last_name: 'TestLastName'
+      )
+      assert user
     end
 
   end
