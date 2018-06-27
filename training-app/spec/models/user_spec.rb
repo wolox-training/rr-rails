27e01b973a2c5aa0a227c9a1f68ce5d5c82ff8e5
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2018-06-27 18:21:25 -0300

    Fixed rubicop tests

--- training-app/spec/models/user_spec.rb
+++ training-app/spec/models/user_spec.rb
@@ -1,19 +1,18 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 describe User do
-  
   describe '.initialize' do
     it 'creates an user with first and last names' do
       user = User.create(
-        email: 'test@wolox.com.ar', 
-        password: '123123123', 
-        password_confirmation: '123123123', 
-        first_name: 'Test', 
+        email: 'test@wolox.com.ar',
+        password: '123123123',
+        password_confirmation: '123123123',
+        first_name: 'Test',
         last_name: 'TestLastName'
       )
       assert user
     end
-
   end
-
 end
