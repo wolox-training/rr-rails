dd1fbf915754a8e84a3494d86c34429a9d7b75fd
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2018-06-28 12:18:57 -0300

    added book model

---
+++ training-app/db/migrate/20180628150240_create_books.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class CreateBooks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :books do |t|
+      t.string :genre, null: false
+      t.string :author, null: false
+      t.string :image, null: false
+      t.string :title, null: false
+      t.string :publisher, null: false
+      t.string :year, null: false
+
+      t.timestamps
+    end
+  end
+end
