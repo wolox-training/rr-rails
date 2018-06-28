d11cd7ef45ff42e16b4a70121be1d61245734b43
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2018-06-28 10:57:48 -0300

    solved Rubocop complains

--- training-app/app/models/user.rb
+++ training-app/app/models/user.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  
   include DeviseTokenAuth::Concerns::User
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable,
-          :confirmable, :omniauthable
-
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable, :omniauthable
 end
