7138ad219626bd93091f1f9931857c2f7ad201cf
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2018-06-28 10:57:48 -0300

    solved Rubocop complains

--- training-app/app/models/user.rb
+++ training-app/app/models/user.rb
@@ -1,9 +1,7 @@
 class User < ApplicationRecord
-  
   include DeviseTokenAuth::Concerns::User
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable,
-          :confirmable, :omniauthable
-
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable, :omniauthable
 end
