0f6ff74d3d4705b4dde85c1b4e217286ba3599f7
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2018-06-28 16:08:17 -0300

    - Added rubocop validation in rspec. - Removed block length for context and describe blocks

--- training-app/spec/rails_helper.rb
+++ training-app/spec/rails_helper.rb
@@ -3,9 +3,8 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 
-
 ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
+require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
@@ -25,7 +24,6 @@ require 'rspec/rails'
 # require only the support files necessary.
 #
 # Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
-
 
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
@@ -67,7 +65,6 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
-
   # Clean up database
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
@@ -78,5 +75,4 @@ RSpec.configure do |config|
       example.run
     end
   end
-
 end
