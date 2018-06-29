90c25d7498e3bed5291b1852d5f7a760ba699381
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2018-06-29 15:47:09 -0300

    Resolved router merge conflicts

--- training-app/db/migrate/20180628123728_devise_token_auth_create_users.rb
+++ training-app/db/migrate/20180628123728_devise_token_auth_create_users.rb
@@ -1,27 +1,12 @@
 class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.2]
   def change
-    create_table(:users) do |t|
+    change_table :users do |t|
       ## Required
       t.string :provider, :null => false, :default => "email"
       t.string :uid, :null => false, :default => ""
 
-      ## Database authenticatable
-      t.string :encrypted_password, :null => false, :default => ""
-
       ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
       t.boolean  :allow_password_change, :default => false
-
-      ## Rememberable
-      t.datetime :remember_created_at
-
-      ## Trackable
-      t.integer  :sign_in_count, :default => 0, :null => false
-      t.datetime :current_sign_in_at
-      t.datetime :last_sign_in_at
-      t.string   :current_sign_in_ip
-      t.string   :last_sign_in_ip
 
       ## Confirmable
       t.string   :confirmation_token
@@ -34,21 +19,12 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.2]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      ## User Info
-      t.string :name
-      t.string :nickname
-      t.string :image
-      t.string :email
-
       ## Tokens
       t.json :tokens
 
-      t.timestamps
     end
 
-    add_index :users, :email,                unique: true
     add_index :users, [:uid, :provider],     unique: true
-    add_index :users, :reset_password_token, unique: true
     add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,       unique: true
   end
