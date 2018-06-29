45442902438509b6d2bc2553164fe622989e23f8
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2018-06-29 17:01:06 -0300

    resolved merge conflicts with master branch

--- training-app/spec/models/book_spec.rb
+++ training-app/spec/models/book_spec.rb
@@ -1,29 +1,9 @@
 require 'rails_helper'
 
 describe Book do
-  context 'validations' do
-    it 'has an author' do
-      is_expected.to validate_presence_of(:author)
-    end
-    it 'has a genre' do
-      is_expected.to validate_presence_of(:genre)
-    end
-    it 'has an image' do
-      is_expected.to validate_presence_of(:image)
-    end
-    it 'has a title' do
-      is_expected.to validate_presence_of(:title)
-    end
-    it 'has a publisher' do
-      is_expected.to validate_presence_of(:publisher)
-    end
-    it 'has a year' do
-      is_expected.to validate_presence_of(:year)
-    end
-  end
 
   subject(:book) do
-    Book.new(
+    described_class.new(
       genre: genre, author: author, title: title, publisher: publisher,
       year: year, image: image
     )
@@ -32,52 +12,71 @@ describe Book do
   let(:genre)  { Faker::Book.genre }
   let(:author) { Faker::Book.author }
   let(:title)  { Faker::Book.title }
-  let(:publisher) { Faker::Book.publisher } 
+  let(:publisher) { Faker::Book.publisher }
   let(:year) { Faker::Number.between(1440, Date.current.year).to_s }
   let(:image) { Faker::Avatar.image }
+
+  it 'has an author' do
+    is_expected.to validate_presence_of(:author)
+  end
+  it 'has a genre' do
+    is_expected.to validate_presence_of(:genre)
+  end
+  it 'has an image' do
+    is_expected.to validate_presence_of(:image)
+  end
+  it 'has a title' do
+    is_expected.to validate_presence_of(:title)
+  end
+  it 'has a publisher' do
+    is_expected.to validate_presence_of(:publisher)
+  end
+  it 'has a year' do
+    is_expected.to validate_presence_of(:year)
+  end
 
   it do
     is_expected.to be_valid
   end
 
   describe '#create' do
-    context 'When genre is nil' do
+    context 'when genre is nil' do
       let(:genre) { nil }
- 
+
       it do
         is_expected.to be_invalid
       end
     end
-    context 'When author is nil' do
+    context 'when author is nil' do
       let(:author) { nil }
 
       it do
         is_expected.to be_invalid
       end
     end
-    context 'When title is nil' do
+    context 'when title is nil' do
       let(:title) { nil }
 
       it do
         is_expected.to be_invalid
       end
     end
-    context 'When publisher is nil' do
+    context 'when publisher is nil' do
       let(:publisher) { nil }
 
       it do
         is_expected.to be_invalid
       end
     end
-    context 'When year is nil' do
+    context 'when year is nil' do
       let(:year) { nil }
 
       it do
         is_expected.to be_invalid
       end
     end
-    context 'When year is nil' do
-      let(:year) { nil }
+    context 'when image is nil' do
+      let(:image) { nil }
 
       it do
         is_expected.to be_invalid
--- training-app/spec/rails_helper.rb
+++ training-app/spec/rails_helper.rb
@@ -1,9 +1,8 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 
-
 ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
+require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
@@ -23,7 +22,6 @@ require 'rspec/rails'
 # require only the support files necessary.
 #
 # Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
-
 
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
@@ -65,7 +63,6 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
-
   # Clean up database
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
@@ -76,5 +73,4 @@ RSpec.configure do |config|
       example.run
     end
   end
-
 end
