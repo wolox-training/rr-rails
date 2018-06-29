ee3c5624bc8fc96454a98a68975726f2e7b67a7c
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2018-06-29 18:23:16 -0300

    Trying to add the sign_in following the guide

--- spec/controllers/api/v1/books_controller_spec.rb
+++ spec/controllers/api/v1/books_controller_spec.rb
@@ -1,3 +1,10 @@
+require 'rails_helper'
+
 describe Api::V1::BooksController do
   include_context 'when logged in'
+
+  it do
+    get '/api/v1/books'
+    expect(response).to have_http_status(:success)
+  end
 end
