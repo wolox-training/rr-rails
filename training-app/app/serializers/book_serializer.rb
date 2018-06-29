ed5a3fafa9490f87651b6e92d29b78f146bf8bae
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2018-06-29 17:46:41 -0300

    Remove frozen string literal checks

--- training-app/app/serializers/book_serializer.rb
+++ training-app/app/serializers/book_serializer.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class BookSerializer < ActiveModel::Serializer
   attributes :author, :genre, :image, :title, :publisher, :year
 end
--- training-app/config/routes.rb
+++ training-app/config/routes.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
   # User authentication
   mount_devise_token_auth_for 'User', at: 'auth'
