f39edbe2b78d63b48e2e5e58ce606fd35f3d852d
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2018-06-29 09:12:17 -0300

    Fixed routes for only index and show

--- training-app/config/routes.rb
+++ training-app/config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
-
-  resources :books
+  # Add only index and show routes
+  resources :books, only: %I[index show]
 end
