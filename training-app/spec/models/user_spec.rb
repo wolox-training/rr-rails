c5db66b1f6066d7e6ae5256e3c2911d114507c28
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2018-06-29 16:48:25 -0300

    changed user spec to use less lines

--- training-app/spec/models/user_spec.rb
+++ training-app/spec/models/user_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 describe User do
   describe '.initialize' do
     it 'creates an user with first and last names' do
-      user = described_class.create(
-        email: 'test@wolox.com.ar', password: '123123123', password_confirmation: '123123123',
+      assert described_class.create(
+        email: 'test@wolox.com.ar',
+        password: '123123123', password_confirmation: '123123123',
         first_name: 'Test', last_name: 'TestLastName'
       )
-      assert user
     end
   end
 end
