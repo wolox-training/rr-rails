f34bb3ad22338bd21225c0a69e10844398cc7053
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2018-06-29 11:40:27 -0300

    Added factory bot for books

---
+++ training-app/spec/factories/book_factory.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :book do
+    genre  { Faker::Book.genre }
+    author { Faker::Book.author }
+    title  { Faker::Book.title }
+    publisher { Faker::Book.publisher }
+    year { Faker::Number.between(1440, Date.current.year).to_s }
+    image { Faker::Avatar.image }
+  end
+end
