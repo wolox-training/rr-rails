c41d7da786a9fe9d5f81862f04e0d61f044b41c2
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2018-06-29 09:12:17 -0300

    Fixed routes for only index and show

--- training-app/config/routes.rb
+++ training-app/config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
-
-  resources :books
+  # Add only index and show routes
+  resources :books, only: %I[index show]
 end
