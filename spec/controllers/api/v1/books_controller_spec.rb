8444e7f75cffcacaf9bdd68948f091089ba5b863
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2018-06-29 18:42:24 -0300

    Added user factory

--- spec/controllers/api/v1/books_controller_spec.rb
+++ spec/controllers/api/v1/books_controller_spec.rb
@@ -1,10 +1,15 @@
 require 'rails_helper'
 
 describe Api::V1::BooksController do
-  include_context 'when logged in'
+  describe '#index' do
+    include_context 'when logged in'
 
-  it do
-    get '/api/v1/books'
-    expect(response).to have_http_status(:success)
+    it do
+      get '/api/v1/books'
+      expect(response).to have_http_status(:success)
+    end
+  end
+  describe '#show' do
+    include_context 'when logged in'
   end
 end
