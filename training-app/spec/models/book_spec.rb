5ce699527ae8d55d82833f2fc51919953936851b
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2018-06-29 17:00:10 -0300

    fixed merge conflicts with spec_helper

--- training-app/spec/models/book_spec.rb
+++ training-app/spec/models/book_spec.rb
@@ -1,5 +1,87 @@
 require 'rails_helper'
 
 describe Book do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    it 'has an author' do
+      is_expected.to validate_presence_of(:author)
+    end
+    it 'has a genre' do
+      is_expected.to validate_presence_of(:genre)
+    end
+    it 'has an image' do
+      is_expected.to validate_presence_of(:image)
+    end
+    it 'has a title' do
+      is_expected.to validate_presence_of(:title)
+    end
+    it 'has a publisher' do
+      is_expected.to validate_presence_of(:publisher)
+    end
+    it 'has a year' do
+      is_expected.to validate_presence_of(:year)
+    end
+  end
+
+  subject(:book) do
+    Book.new(
+      genre: genre, author: author, title: title, publisher: publisher,
+      year: year, image: image
+    )
+  end
+
+  let(:genre)  { Faker::Book.genre }
+  let(:author) { Faker::Book.author }
+  let(:title)  { Faker::Book.title }
+  let(:publisher) { Faker::Book.publisher } 
+  let(:year) { Faker::Number.between(1440, Date.current.year).to_s }
+  let(:image) { Faker::Avatar.image }
+
+  it do
+    is_expected.to be_valid
+  end
+
+  describe '#create' do
+    context 'When genre is nil' do
+      let(:genre) { nil }
+ 
+      it do
+        is_expected.to be_invalid
+      end
+    end
+    context 'When author is nil' do
+      let(:author) { nil }
+
+      it do
+        is_expected.to be_invalid
+      end
+    end
+    context 'When title is nil' do
+      let(:title) { nil }
+
+      it do
+        is_expected.to be_invalid
+      end
+    end
+    context 'When publisher is nil' do
+      let(:publisher) { nil }
+
+      it do
+        is_expected.to be_invalid
+      end
+    end
+    context 'When year is nil' do
+      let(:year) { nil }
+
+      it do
+        is_expected.to be_invalid
+      end
+    end
+    context 'When year is nil' do
+      let(:year) { nil }
+
+      it do
+        is_expected.to be_invalid
+      end
+    end
+  end
 end
--- training-app/spec/rails_helper.rb
+++ training-app/spec/rails_helper.rb
@@ -1,5 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
@@ -21,6 +23,14 @@ require 'rspec/rails'
 # require only the support files necessary.
 #
 # Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
@@ -54,4 +64,17 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+
+  # Clean up database
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
 end
