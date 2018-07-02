7973dbc7fabca90478e812a047f384ae9c5e9a2b
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2018-07-02 17:53:15 -0300

    Removed broken gem

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -1,10 +1,11 @@
 class NotificationsMailer < ApplicationMailer
-  include Sidekiq::Mailer
-
   def new_rent_email(rent)
     @rent = rent
     @user = rent.user
     @book = rent.book
-    mail to: @user.email, subject: 'New rent notification from WBooks', &:html
+    mail(to: @user.email, subject: 'New rent notification from WBooks') do |config|
+      config.html
+      config.text
+    end
   end
 end
