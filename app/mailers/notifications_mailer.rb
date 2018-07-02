c17d32980857157140e16c5be9f6b34f1b90924e
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2018-07-02 17:53:15 -0300

    Removed broken gem

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -3,6 +3,9 @@ class NotificationsMailer < ApplicationMailer
     @rent = Rent.find(rent_id)
     @user = rent.user
     @book = rent.book
-    mail to: @user.email, subject: 'New rent notification from WBooks', &:html
+    mail(to: @user.email, subject: 'New rent notification from WBooks') do |config|
+      config.html
+      config.text
+    end
   end
 end
