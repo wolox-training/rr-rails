9b26986ea4af34fe7f7bfae384d7682647eb4bb5
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2018-07-03 16:25:02 -0300

    Added base policies

---
+++ app/policies/application_policy.rb
@@ -0,0 +1,61 @@
+class ApplicationPolicy
+  attr_reader :user, :record
+
+  def initialize(user, record)
+    @user = user
+    @record = record
+  end
+
+  def index?
+    false
+  end
+
+  def show?
+    false
+  end
+
+  def create?
+    false
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    false
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    false
+  end
+
+  def scope
+    Pundit.policy_scope!(user, record.class)
+  end
+
+  def belongs_to_user?
+    record.user_id == user.id
+  end
+
+  def admin?
+    user.admin?
+  end
+
+  class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      scope.all
+    end
+  end
+end
---
+++ app/policies/rent_policy.rb
@@ -0,0 +1,2 @@
+class RentPolicy < ApiPolicy
+end
