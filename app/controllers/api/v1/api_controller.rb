d8f08366ccc199766ae66d66f4f369734b2a5676
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2018-07-03 11:45:05 -0300

    added locale on user and force locale change based on user preference

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -3,8 +3,16 @@ module Api
     class ApiController < ApplicationController
       # All API controllers require to have the user authenticated
       before_action :authenticate_user!
+      # We set the current locale for our user
+      before_action :set_user_locale
       # All API controllers are paginated
       include Wor::Paginate
+
+      protected
+
+      def set_user_locale
+        I18n.locale = current_user.locale.to_sym || I18n.default_locale
+      end
     end
   end
 end
