b28e1c227fa758bb6c16989af251bfe6132c0194
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2018-07-03 11:25:40 -0300

    fixed more merge conflicts

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,8 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-<<<<<<< HEAD
   default from: RAILS.application.secrets.mailer_address
-=======
-  default from: ENV['no-reply@training.wolox.com.ar']
->>>>>>> 7e19dee044673882f06d3b04ca3a67fa01aabf6f
   layout 'mailer'
 end
