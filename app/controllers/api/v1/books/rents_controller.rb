3b9215cd7eb15c4dfd10563d26fb0e345e4b7901
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2018-07-03 16:30:38 -0300

    Added authorization Removed book policy because it wasn't in use

--- app/controllers/api/v1/books/rents_controller.rb
+++ app/controllers/api/v1/books/rents_controller.rb
@@ -3,7 +3,7 @@ module Api
     module Books
       class RentsController < ApiController
         def index
-          render_paginated Rent.where(book_id: params[:book_id])
+          render_paginated policy_scope(Rent).where(book_id: params[:book_id])
         end
       end
     end
