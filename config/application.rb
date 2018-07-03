2d658b88d78b2a033f25dabc2ee651db0019950c
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2018-07-03 11:04:36 -0300

    Using config secrets properly now

--- config/application.rb
+++ config/application.rb
@@ -17,12 +17,12 @@ module WBooks
     # the framework and any gems in your application.
     config.active_job.queue_adapter = :sidekiq
     config.action_mailer.smtp_settings = {
-      domain: ENV['MAILER_DOMAIN'],
-      username: ENV['MAILER_USERNAME'],
-      password: ENV['MAILER_PASSWORD'],
-      address: ENV['MAILER_ADDRESS'],
-      port: ENV['MAILER_PORT'],
-      authentication: ENV['MAILER_AUTH']
+      domain: Rails.secrets.mailer_domain,
+      username: Rails.secrets.mailer_user,
+      password: Rails.secrets.mailer_password,
+      address: Rails.secrets.mailer_address,
+      port: Rails.secrets.mailer_port,
+      authentication: Rails.secrets.mailer_auth
     }
   end
 end
