bed883aab9bdaeaf3412d2b75e0440bcfbcc5219
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2018-07-05 17:03:15 -0300

    Solved merge conflicts

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -1,8 +1,10 @@
 class NotificationsMailer < ApplicationMailer
+  include Sidekiq::Mailer
+
   def new_rent_email(rent)
     @rent = rent
     @user = rent.user
     @book = rent.book
-    mail(to: @user.email, subject: 'New rent notification from WBooks', &:html)
+    mail to: @user.email, subject: 'New rent notification from WBooks', &:html
   end
 end
