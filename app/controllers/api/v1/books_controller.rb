bc01ce07ed26ebbbbd5276ef4eaec50c7327400c
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2018-07-05 16:46:08 -0300

    book controller now returns 404 when a record wasn't found

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -7,8 +7,15 @@ module Api
 
       def show
         # Not a strong parameter, because :id is part of the API URL
-        @book = Book.find(params[:id])
-        render json: @book
+        respond_to do |format|
+          if Book.exists? params[:id]
+            format.json { render json: Book.find(params[:id]) }
+          else
+            format.json do
+              render status: 404, json: { error: "No book was found with id #{params[:id]}." }
+            end
+          end
+        end
       end
     end
   end
