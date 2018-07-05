4b38b9dd384769422f84d955efa74053ae22b161
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2018-07-05 18:07:25 -0300

    Added filters to ActiveAdmin

--- app/admin/rents.rb
+++ app/admin/rents.rb
@@ -1,2 +1,15 @@
 ActiveAdmin.register Rent do
+  permit_params :from, :to, :book, :user
+
+  index do
+    selectable_column
+    id_column
+    column :from
+    column :to
+    column :user
+    column :book
+    actions
+  end
+
+  filter :created_at
 end
