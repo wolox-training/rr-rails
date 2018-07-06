155dbbfa12d6371b4b0555e665619fa438f97802
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2018-07-06 09:21:24 -0300

    Added relationships to BookSuggestion and User

--- app/models/book_suggestion.rb
+++ app/models/book_suggestion.rb
@@ -2,6 +2,6 @@ class BookSuggestion < ApplicationRecord
   validates :author, presence: true
   validates :title, presence: true
   validates :link, presence: true
-
-  belongs_to :user
+  # Next card specifies than a suggestion can be made without a book
+  belongs_to :user, optional: true 
 end
