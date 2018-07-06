6e0ab05998f8f9edb25934357d4cf85016e0759e
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2018-07-06 09:32:49 -0300

    rewritten controller to respond properly

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -2,10 +2,15 @@ module Api
   module V1
     class SearchController < ApplicationController
       def index
-        service = OpenLibraryService.new
-        result = service.book_info search_params['isbn']
+        result = OpenLibraryService.new.book_info search_params['isbn']
         respond_to do |format|
-          format.json { render json: result }
+          if result
+            format.json { render json: result }
+          else
+            format.json do
+              render status: 404, json: { ok: false, error: 'No book was found with that ISBN.' }
+            end
+          end
         end
       end
 
