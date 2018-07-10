ddd98f16a9b6a6301da03108d782743480852ea2
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2018-07-10 12:58:14 -0300

    removed unnecesary line

--- app/controllers/devise_token_auth_controller.rb
+++ app/controllers/devise_token_auth_controller.rb
@@ -1,6 +1,4 @@
 class DeviseTokenAuthController < ApplicationController
   include Pundit
   include DeviseTokenAuth::Concerns::SetUserByToken
-  # Optional authentication
-  before_filter :authenticate_user
 end
