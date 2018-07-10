aee505979211eb8fd7954584dae2021f19de00e8
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2018-07-10 12:36:33 -0300

    Added optional authentication

--- app/controllers/devise_token_auth_controller.rb
+++ app/controllers/devise_token_auth_controller.rb
@@ -1,4 +1,6 @@
 class DeviseTokenAuthController < ApplicationController
   include Pundit
   include DeviseTokenAuth::Concerns::SetUserByToken
+  # Optional authentication
+  before_filter :authenticate_user
 end
